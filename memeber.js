aa8a3eb453ac4e32926b827181fc67731d564499
Raichalpj <86045317+Raichalpj@users.noreply.github.com>
2024-05-14 11:44:55 +0000

    copilot second commit

---
+++ memeber.js
@@ -0,0 +1,9 @@
+function skillsMember(){
+    console.log('You have the following skills: ');
+    console.log('1. JavaScript');
+    console.log('2. Python');
+    console.log('3. SQL');
+    console.log('4. NodeJS');
+    console.log('5. React');
+    console.log('6. Express');
+}
